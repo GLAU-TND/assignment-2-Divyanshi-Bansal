c0f9523e32548da3f01d7dca16a3eeb81d9a6936
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-27 23:41:27 +0530

    make contructor in MyCircularQueue.java

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,7 +5,22 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private int size;
+    private int maxsize;
+    private int front;
+    private int rear;
+    private Student[] circularQueue;
 
+    public MyCircularQueue(int maxsize){
+        this.maxsize = maxsize;
+        this.front =0;
+        this.rear = 0;
+        this.size =0;
+        circularQueue = new Student[maxsize];
+    }
 }
