f0c637829f9d522cdb15a41b98c8f5e2b68fe35b
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-27 22:42:32 +0530

    make getter and setter in LinkedList.java

--- src/problem4/linkedList/LinkedList.java
+++ src/problem4/linkedList/LinkedList.java
@@ -40,4 +40,20 @@ public class LinkedList {
         }
         return null;
     }
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node first) {
+        this.first = first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
 }
