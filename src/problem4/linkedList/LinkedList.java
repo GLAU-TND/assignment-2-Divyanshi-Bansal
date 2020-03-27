a987579fd9edf9aada7c50a986ef3822d9780198
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-27 22:39:51 +0530

    make display method in LinkedList.java

--- src/problem4/linkedList/LinkedList.java
+++ src/problem4/linkedList/LinkedList.java
@@ -21,4 +21,12 @@ public class LinkedList {
         first.setNext ( null );
         first = second;
     }
+
+    public void display(){
+        var current = first;
+        while(current != null){
+            System.out.println (current.getData ());
+            current = current.getNext ();
+        }
+    }
 }
