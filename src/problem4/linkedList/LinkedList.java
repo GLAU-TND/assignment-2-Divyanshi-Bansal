a5ad3c67fb4059870900fbd9a78d63c97514f508
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-27 22:36:57 +0530

    make addlast method in LinkedList.java

--- src/problem4/linkedList/LinkedList.java
+++ src/problem4/linkedList/LinkedList.java
@@ -1,5 +1,18 @@
 package problem4.linkedList;
 
 public class LinkedList {
+    private Node first;
+    private Node last;
 
+    public void addLast(int data){
+        Node node = new Node ( data );
+        if(first == null ){
+            first = last = node;
+            return;
+        }
+        else{
+            last.setNext ( node );
+            last = node;
+        }
+    }
 }
