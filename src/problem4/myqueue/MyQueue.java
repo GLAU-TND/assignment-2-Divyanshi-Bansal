4d4224bc48216302c7369ac71b5f22800f765945
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-27 22:46:48 +0530

    make peek method in MyQueue.java

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,12 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.linkedList.LinkedList;
+
 public class MyQueue {
+    LinkedList linkedList = new LinkedList ();
+
+    public void enqueue(int data){
+        linkedList.addLast ( data );
+    }
 }
