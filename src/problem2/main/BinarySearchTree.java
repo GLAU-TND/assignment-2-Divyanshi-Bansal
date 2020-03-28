909407d1a1b444595927b556f67f604a09e3e2b1
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-28 17:54:18 +0530

    create constructor

--- src/problem2/main/BinarySearchTree.java
+++ src/problem2/main/BinarySearchTree.java
@@ -1,5 +1,17 @@
 package problem2.main;
 
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
 public class BinarySearchTree {
+    private TreeNode root;
+    private int countRight;
+    private MyQueue preQueue;
+    private MyQueue postQueue;
+
+    public BinarySearchTree (MyQueue preQueue , MyQueue postQueue){
+        this.postQueue = postQueue;
+        this.preQueue = preQueue;
+    }
 
 }
