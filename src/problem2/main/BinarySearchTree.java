9fb6a043b26befb4582465cedb88102ef8517127
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-28 18:05:02 +0530

    create postOrder method

--- src/problem2/main/BinarySearchTree.java
+++ src/problem2/main/BinarySearchTree.java
@@ -74,4 +74,15 @@ public class BinarySearchTree {
             preOrder ( temp.getRight () );
         }
     }
+
+    public void postOrder(TreeNode temp){
+        if(temp == null){
+            return;
+        }
+        else{
+            postOrder ( temp.getLeft () );
+            postOrder ( temp.getRight () );
+            postQueue.enqueue ( temp.getData () );
+        }
+    }
 }
