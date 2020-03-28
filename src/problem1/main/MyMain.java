df3bb1b14d2ad8dd4359bebb6b7d2dc387930519
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-03-28 18:42:12 +0530

    completed MyMain.java of prob1

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,8 +7,22 @@
 package problem1.main;
 // executable class
 
- class Mymain{
-     public static void main(String[] args) {
+import problem1.mybst.MyBinarySearchTree;
 
+class Mymain{
+     public static void main(String[] args) {
+         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree ();
+         myBinarySearchTree.insert ( 14 );
+         myBinarySearchTree.insert ( 13 );
+         myBinarySearchTree.insert ( 15 );
+         myBinarySearchTree.insert ( 74 );
+         myBinarySearchTree.insert ( 10 );
+         myBinarySearchTree.insert ( 24 );
+         myBinarySearchTree.insert ( 4 );
+
+         System.out.println ("left children of the binary search tree: ");
+         myBinarySearchTree.traverse ();
+         System.out.println ("no. of parents not having their left child are:");
+         System.out.println (myBinarySearchTree.getCountRight ());
      }
 }
